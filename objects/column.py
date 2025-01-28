17182fcb9e0c579cd803325e5e8c80bd405fba37
johnnybasgallop <johnnybasgallopwork@outlook.com>
2025-01-28 17:41:19 +0000

    tuned parameters for more difficult gameplay

--- objects/column.py
+++ objects/column.py
@@ -10,7 +10,7 @@ from layer import Layer
 class Column(pygame.sprite.Sprite):
     def __init__(self, *groups):
         self._layer = Layer.OBSTACLE
-        self.gap = 100
+        self.gap = 93
 
         self.sprite = assets.get_sprite("pipe-green")
         self.sprite_rect = self.sprite.get_rect()
